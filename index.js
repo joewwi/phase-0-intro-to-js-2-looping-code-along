b58c0159c0e21f6e8077fc59a614489fcaec2dec
Martin <joewwi03@gmail.com>
2023-03-19 16:44:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(string_name, event_name)
+{
+    var array_store = []; 
+    for (let counter = 0; counter < string_name.length; counter++)
+    {    
+        array_store.push(`Thank you, ${string_name[counter]}, for the wonderful ${event_name} gift!`);
+
+    }
+    return array_store;
+}
+
+ function countDown(integer)
+ {
+
+    while (integer >= 0)
+    {
+        console.log(integer)
+        integer--;
+    }
+ }
